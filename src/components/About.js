652e34e076d321df5f029600b293ef3fb97a1c7f
TYSON BECK <tysonbeck@TYSONs-iMac.local>
2022-02-13 12:37:13 -0800

    2022 Update

--- src/components/About.js
+++ src/components/About.js
@@ -3,20 +3,20 @@ import Fade from 'react-reveal/Fade';
 import data from '../data';
 
 class About extends Component {
-    state = {  }
-    render() { 
+    state = {}
+    render() {
         return (<div className='about'>
             <div className='about-content'>
-            <h1><Fade bottom cascade>About.</Fade></h1>
-            <Fade bottom>
+                <h1><Fade bottom cascade>About.</Fade></h1>
+                <Fade bottom>
                     <p>{data.abouttext}</p>
-            </Fade>
+                </Fade>
             </div>
             {data.ShowAboutImage ? <img src={data.aboutImage} alt='about iamge'></img> : null}
-            
 
-        </div>  );
+
+        </div>);
     }
 }
- 
+
 export default About;
--- src/data.js
+++ src/data.js
@@ -1,22 +1,22 @@
 export default
     {
-        name :'Tyson Vonn Beck.',
+        name: 'Tyson Vonn Beck.',
         headerTagline: [
-                        'Hi 👋 I am Tyson Vonn Beck.',
-                        'Full-Stack Web Engineer',
-                        'from Seattle'
-    ],
-        contactEmail:'tysonvonnbeck@msn.com',
-        abouttext: "I have over 15 years experience in IT, primarily in Quality Assurance with some time as an Assitant Producer. I've loved working in games, both for console and for social/mobile. I hope to add to my journey in tech with my new skills in web development.",
-        aboutImage:'https://tysonvonnbeck.github.io/updatedportfolio/images/Profile.png',
-       ShowAboutImage:true,
-       projects:[
-           {
-            id: 1,
-            title:'Spontaneity',
-             service:'Front-End Design, Back-End Development', 
-             imageSrc:"https://res.cloudinary.com/tyson/image/upload/v1583357354/projects/adventure_jf9mw2.png",
-             url:'https://aqueous-savannah-60861.herokuapp.com/'
+            'Hi 👋 I am Tyson Vonn Beck.',
+            'Full-Stack Web Engineer',
+            'from Seattle'
+        ],
+        contactEmail: 'tysonvonnbeck@msn.com',
+        abouttext: "I have over 10 years experience in IT, primarily in Quality Assurance with some time as an Assitant Producer. I've loved working in games, both for console and for social/mobile. I hope to add to my journey in tech with my new skills in web development.",
+        aboutImage: 'https://tysonvonnbeck.github.io/updatedportfolio/images/Profile.png',
+        ShowAboutImage: true,
+        projects: [
+            {
+                id: 1,
+                title: 'Spontaneity',
+                service: 'Front-End Design, Back-End Development',
+                imageSrc: "https://res.cloudinary.com/tyson/image/upload/v1583357354/projects/adventure_jf9mw2.png",
+                url: 'https://aqueous-savannah-60861.herokuapp.com/'
             },
             {
                 id: 2,
@@ -25,7 +25,7 @@ export default
                 imageSrc: "https://res.cloudinary.com/tyson/image/upload/v1583357354/projects/Top_3_ga72eg.png",
                 url: 'https://top3recommendations.herokuapp.com/'
             },
-            { 
+            {
                 id: 3,
                 title: 'Parlor Flix',
                 service: 'Front-End Design',
@@ -38,19 +38,21 @@ export default
                 service: 'Full Development',
                 imageSrc: "https://res.cloudinary.com/tyson/image/upload/v1583357353/projects/RPWG_hq8ymv.png",
                 url: 'https://tysonvonnbeck.github.io/RandomPWgen/'
-           }
+            }
 
         ],
         social: [
-            {   name:'Github',
-                url:'https://github.com/tysonvonnbeck'},
+            {
+                name: 'Github',
+                url: 'https://github.com/tysonvonnbeck'
+            },
             {
                 name: 'LinkedIn',
                 url: 'https://www.linkedin.com/in/tyson-beck-0120b29b/'
             },
             {
                 name: 'Resume',
-                url: 'https://tysonvonnbeck.github.io/updatedportfolio/documents/Tyson%20Vonn%20Beck%202020.pdf'
+                url: 'https://github.com/tysonvonnbeck/updatedportfolio/tree/master/documents#:~:text=.%E2%80%8A.-,Tyson%20Beck%202022.pdf'
             },
             {
                 name: 'Writing',
